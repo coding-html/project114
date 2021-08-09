4ccf459b455d1d450be73471c3cf464319edbc2d
coding-html <74841670+coding-html@users.noreply.github.com>
2021-08-09 15:55:22 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() 
+{
+ canvas=createCanvas(300,300)
+  canvas.center();
+  canvas.position(300,200);
+}
+
+function draw() {
+
+}
+function preload()
+{
+  
+}
+function save()
+{
+  
+}
